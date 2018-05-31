2d6bce27cd36a6b9b6f18307b0a1e93712da5dfb
Christophe Alladoum <christophe.alladoum@sophos.com>
2018-05-31 11:54:51 -0700

    moved to VScode + started DoubleFetch

---
+++ hevd.h
@@ -0,0 +1,17 @@
+#define DRIVER_PATH "\\\\.\\HackSysExtremeVulnerableDriver"
+
+/**
+ * IOCTL code for HEVD 1.20
+ */
+#define IOCTL_HEVD_TYPE_CONFUSION       0x222023
+#define IOCTL_HEVD_INTEGER_OVERFLOW     0x22201f
+#define IOCTL_HEVD_NULL_POINTER         0x22201b
+#define IOCTL_HEVD_UNITIALIZED_STACK    0x222017
+#define IOCTL_HEVD_UNITIALIZED_HEAP     0x222013
+#define IOCTL_HEVD_DOUBLE_FETCH         0x22200f
+#define IOCTL_HEVD_STACK_OVERFLOW       0x222003 // done
+#define IOCTL_HEVD_STACK_OVERFLOW_GS    0x221fff
+#define IOCTL_HEVD_ARBITRARY_OVERWRITE  0x221ffb // done
+#define IOCTL_HEVD_ALLOCATE_UAF         0x221ff7
+#define IOCTL_HEVD_FREE_UAF             0x221ff3
+#define IOCTL_HEVD_USE_UAF              0x221fef
