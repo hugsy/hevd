c337b9278ddb42190b65739c3259409d256a0a74
hugsy <hugsy@blah.cat>
2018-06-25 23:50:30 -0700

    moved old exploits to win8.1 folder + added win10 folder

--- hevd.h
+++ hevd.h
@@ -8,10 +8,10 @@
 #define IOCTL_HEVD_NULL_POINTER         0x22202b
 #define IOCTL_HEVD_UNITIALIZED_STACK    0x22202f
 #define IOCTL_HEVD_UNITIALIZED_HEAP     0x222033
-#define IOCTL_HEVD_DOUBLE_FETCH         0x222037 // done
-#define IOCTL_HEVD_STACK_OVERFLOW       0x222003 // done
+#define IOCTL_HEVD_DOUBLE_FETCH         0x222037 // done win8
+#define IOCTL_HEVD_STACK_OVERFLOW       0x222003 // done win8
 #define IOCTL_HEVD_STACK_OVERFLOW_GS    0x221fff
-#define IOCTL_HEVD_ARBITRARY_OVERWRITE  0x221ffb // done
+#define IOCTL_HEVD_ARBITRARY_OVERWRITE  0x221ffb // done win8
 #define IOCTL_HEVD_ALLOCATE_UAF         0x221ff7
 #define IOCTL_HEVD_FREE_UAF             0x221ff3
 #define IOCTL_HEVD_USE_UAF              0x221fef
